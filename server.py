d34e6ac5b329a533a436ec2265ccda5ad62d337c
Nithin Daniel <nithindaniel2018@gmail.com>
2024-07-14 00:00:17 +0530

    feat:added html to pdf

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ CORS(app)
 # Configuration
 UPLOAD_FOLDER = 'uploads'
 OUTPUT_FOLDER = 'output'
-ALLOWED_EXTENSIONS = {'xlsx','docx'}
+ALLOWED_EXTENSIONS = {'xlsx','docx','html'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
@@ -35,6 +35,16 @@ def convert_xlsx_to_pdf(xlsx_path, output_dir):
 def convert_docx_to_pdf(docx_path, output_dir):
     try:
         command = ['libreoffice', '--headless', '--convert-to', 'pdf', docx_path, '--outdir', output_dir]
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if result.returncode != 0:
+            return False, result.stderr.decode()
+        return True, None
+    except Exception as e:
+        return False, str(e)
+    
+def convert_html_to_pdf(html_path, output_dir):
+    try:
+        command = ['libreoffice', '--headless', '--convert-to', 'pdf', html_path, '--outdir', output_dir]
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if result.returncode != 0:
             return False, result.stderr.decode()
@@ -93,6 +103,34 @@ def upload_docx_file():
 
         # Convert DOCX to PDF
         success, error = convert_docx_to_pdf(file_path, app.config['OUTPUT_FOLDER'])
+        if not success:
+            return jsonify(error=error), 500
+
+        pdf_filename = filename.rsplit('.', 1)[0] + '.pdf'
+        pdf_path = os.path.join(app.config['OUTPUT_FOLDER'], pdf_filename)
+        os.remove(file_path)
+        return send_file(pdf_path, as_attachment=True, download_name=pdf_filename)
+
+    return jsonify(error="Invalid file type"), 400
+
+@app.route('/html-to-pdf', methods=['POST'])
+@cross_origin(origins=['http://inovuslabs.org',''])
+def upload_html_file():
+    if 'file' not in request.files:
+        return jsonify(error="No file part"), 400
+
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify(error="No selected file"), 400
+    
+    if file and file.filename.lower().endswith('.html'):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        # Convert HTML to PDF
+        success, error = convert_html_to_pdf(file_path, app.config['OUTPUT_FOLDER'])
         if not success:
             return jsonify(error=error), 500
 
